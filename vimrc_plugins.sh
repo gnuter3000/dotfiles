5e742115a9609afbee04a390bcf46d8680cb4ae6
gnuter3000 <gunther.weissenbaeck@muellerboon.de>
2018-04-13 11:09:43 +0200

    vimrc completed

---
+++ vimrc_plugins.sh
@@ -0,0 +1,11 @@
+" mkdir -p ~/.vim/colors && cd ~/.vim/colors
+" mkdir -p ~/.vim/autoload ~/.vim/bundle
+" mkdir -p ~/.vim/ftplugin
+" wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
+" wget -O ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
+" curl -so ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+" git clone git://github.com/Lokaltog/vim-powerline.git
+" git clone https://github.com/kien/ctrlp.vim.git
+" git clone https://github.com/klen/python-mode
+" git clone git://github.com/davidhalter/jedi-vim.git
+" curl -so ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
