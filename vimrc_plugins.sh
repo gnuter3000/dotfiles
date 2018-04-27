55f5947df8a81ea6dbfae9d1381a1429cbf300ee
gnuter3000 <gunther.weissenbaeck@muellerboon.de>
2018-04-27 15:46:52 +0200

    updated the script

--- vimrc_plugins.sh
+++ vimrc_plugins.sh
@@ -7,16 +7,16 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -so ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 cd ~/.vim/bundle
-git clone git://github.com/Lokaltog/vim-powerline.git
+git clone https://github.com/Lokaltog/vim-powerline.git
 
 cd ~/.vim/bundle
 git clone https://github.com/kien/ctrlp.vim.git
 
-# cd ~/.vim/bundle
-# git clone https://github.com/klen/python-mode
+cd ~/.vim/bundle
+git clone https://github.com/klen/python-mode
 
 cd ~/.vim/bundle
-git clone git://github.com/davidhalter/jedi-vim.git
+git clone https://github.com/davidhalter/jedi-vim.git
 
 mkdir -p ~/.vim/ftplugin
 wget -O ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
